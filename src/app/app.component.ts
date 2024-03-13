65d487a8ea22105721539b09c555b96e56296229
NBguys <gudwns520@gamil.com>
2024-03-13 15:56:01 +0900

    커스텀 속성에 바인딩 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type:'server', content:'내용', name:'이름' }];
+  serverElements = [{type:'server',  name:'이름', content:'내용' }];
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
